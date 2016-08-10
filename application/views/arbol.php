01b11499763715cdd67f67c801c79635a7474e10
panxo316 <francisco.cordero.c@gmail.com>
2016-08-10 14:42:26 -0400

    Cambio de visualizacion

--- application/views/arbol.php
+++ application/views/arbol.php
@@ -1,24 +1,29 @@
-	<div class="tree well">
+	
+	<!-- <div class="tree well">
     <ul>
        <li> 
 	
-	<?php
-		foreach ($proyectos as $data_proyectos){
-		// echo $data->P_ID_PROYECTO."<br>";
-		echo "<span><i class='fa fa-folder-open'></i>".$data_proyectos->P_NOMBRE_PROYECTO."</span> <a href='' style='display:none;'>Goes somewhere</a>";
-		foreach ($hitos as $data_hitos){
-		echo "<ul><li>";
-		echo "<span><i class='fa fa-minus'></i>".$data_hitos->P_NOMBRE_HITO."</span> <a href='' style='display:none;'>Goes somewhere</a></li>";
+	<?php /*
+		foreach ($proyectos as $data_proyectos){ ?>
+		<span><i class='fa fa-folder-open'></i><?php echo $data_proyectos->P_NOMBRE_PROYECTO; ?></span> <a href='' style='display:none;'>Goes somewhere</a>
+		<ul>
+		<li>
+				<?php foreach ($hitos as $data_hitos){ ?>
+						<span><i class='fa fa-minus'></i><?php echo $data_hitos->P_NOMBRE_HITO; ?></span> <a href='' style='display:none;'>Goes somewhere</a>
+						<ul>
+							<?php foreach ($actividades as $data_actividades){
+								if($data_hitos->P_ID_HITO == $data_actividades->P_ID_HITO){ ?>
+						       <li><span><i class='fa fa-minus'></i><?php echo $data_actividades->P_NOMBRE_ACTIVIDAD; ?></span> <a href='' style='display:none;'>Goes somewhere</a></li>
+								<?php } 
+								}
+								?>
+							
+							</ul>
+			<?php	} ?>
 		
-				foreach ($actividades as $data_actividades){
-					if($data_hitos->P_ID_HITO == $data_actividades->P_ID_HITO){
-					echo "<ul><li><span><i class='fa fa-minus'></i>".$data_actividades->P_NOMBRE_ACTIVIDAD."</span> <a href='' style='display:none;'>Goes somewhere</a></li></ul>";
-					}
-				}
-		echo "</li></ul>";
-		}
-		echo "</ul>";
-		
+			</li>
+		</ul>
+<?php		
 		// echo $data->P_ID_USUARIO_RESPONSABLE."<br>";
 		// echo $data->P_FECHA_INICIO."<br>";
 		// echo $data->P_FECHA_TERMINO."<br>";
@@ -28,55 +33,56 @@
 		
 	}
 	
-	?>
+	*/ ?>
 </li>
 </ul>
 </div>
-	
-	
-	
-	
-	<div class="tree well">
+-->
+
+<div class="container">
+  <h1>Proyecto</h1>
+  <?php foreach ($proyectos as $data_proyectos){ ?>
+  <div class="tree">
     <ul>
-        <li>
-            <span><i class="fa fa-folder-open"></i> Item N°1: Construccion de Software</span> <a href="" style="display:none;">Goes somewhere</a>
+      <li>
+        <a href="#"><?php echo $data_proyectos->P_NOMBRE_PROYECTO; ?></a>
+        <ul>
+		<?php foreach ($hitos as $data_hitos){ ?>
+          <li>
+            <a href="#"><?php echo $data_hitos->P_NOMBRE_HITO; ?></a>
             <ul>
-                <li>
-                	<span><i class="fa fa-minus"></i> Evaluacion de Requerimientos</span> <a href="" style="display:none;">Goes somewhere</a>
-					<ul>
-						<li>
-						<span><i class="fa fa-minus "></i> Clasificacion de Requerimientos | <label class="control-label">Fecha Inicio:</label>10/08/2016 <label class="control-label">Fecha Termino:</label>20/08/2016 </span><a href="" style="display:none;">Goes somewhere</a>
-						</li>
-						<li>
-						<span><i class="fa fa-minus "></i> Asignacion de Requerimientos | <label class="control-label">Fecha Inicio:</label>10/08/2016 <label class="control-label">Fecha Termino:</label>20/08/2016 </span><a href="" style="display:none;">Goes somewhere</a>
-						</li>
-					</ul>
-                </li>
-                <li>
-	                <span><i class="fa fa-leaf"></i> Diseño de Solucion</span> <a href="" style="display:none;">Goes somewhere</a>
-					<ul>
-						<li>
-						<span><i class="fa fa-minus "></i> Diseño grafico de sistema | <label class="control-label">Fecha Inicio:</label>10/08/2016 <label class="control-label">Fecha Termino:</label>20/08/2016 </span><a href="" style="display:none;">Goes somewhere</a>
-						</li>
-						<li>
-						<span><i class="fa fa-minus "></i> Diseño de Funcionalidades | <label class="control-label">Fecha Inicio:</label>10/08/2016 <label class="control-label">Fecha Termino:</label>20/08/2016 </span><a href="" style="display:none;">Goes somewhere</a>
-						</li>
-					</ul>
-                </li>
-				<li>
-                	<span><i class="fa fa-minus "></i> Desarrollo de Prototipo</span> <a href="" style="display:none;">Goes somewhere</a>
-					<ul>
-						<li>
-						<span><i class="fa fa-minus "></i> Implantacion de grafica de sistema | <label class="control-label">Fecha Inicio:</label>10/08/2016 <label class="control-label">Fecha Termino:</label>20/08/2016 </span> <a href="" style="display:none;">Goes somewhere</a>
-						</li>
-						<li>
-						<span><i class="fa fa-minus "></i> Desarrollo de Funcionalidades | <label class="control-label">Fecha Inicio:</label>10/08/2016 <label class="control-label">Fecha Termino:</label>20/08/2016  </span><a href="" style="display:none;">Goes somewhere</a>
-						</li>
-					</ul>
-                </li>
-                    
-              
+			<?php foreach ($actividades as $data_actividades){ 
+				if($data_hitos->P_ID_HITO == $data_actividades->P_ID_HITO){ ?>
+              <li>	<a href="#"><?php echo $data_actividades->P_NOMBRE_ACTIVIDAD." - Fecha Inicio:". $data_actividades->P_FECHA_INICIO." / Fecha TÃ©rmino:". $data_actividades->P_FECHA_TERMINO." / Valor Asignado:". $data_actividades->P_VALOR ." / Avance:". $data_actividades->P_PORC_AVANCE."%" ?></a>
+              </li>
+			<?php }	} ?>
             </ul>
-        </li>
+          </li>
+		   <?php	} ?>
+        <!--  <li>
+            <a href="#">Child</a>
+            <ul>
+              <li><a href="#">Grand Child</a>
+              </li>
+              <li>
+                <a href="#">Grand Child</a>
+                <ul>
+                  <li>	<a href="#">Great Grand Child</a>
+                  </li>
+                  <li>	<a href="#">Great Grand Child</a>
+                  </li>
+                  <li>	<a href="#">Great Grand Child</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="#">Grand Child</a>
+              </li>
+            </ul>
+          </li>
+		  -->
+        </ul>
+      </li>
     </ul>
+  </div>
+  <?php	} ?>
 </div>
