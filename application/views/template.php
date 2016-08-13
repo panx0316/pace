0a4e096e32badcf04d0e8d39c68cccb5c43b7373
panxoweb <francisco.cordero.c@gmail.com>
2016-08-13 19:04:33 -0400

    Bajada de archivos CDN a servidor
    Ahora las librerias que se llamaban de la red, ahora estan alojadas en
    el proyecto, por lo tanto se redujo el tiempo de carga, y se incremento
    el rendimiento del sitio.

--- application/views/template.php
+++ application/views/template.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<?php $ruta='http://localhost:8080/pace/';
+<?php $ruta='http://localhost/pace/';
 //$ruta='http://localhost/pace/' 
 
 define('RUTA', $ruta);
@@ -10,22 +10,22 @@ define('RUTA', $ruta);
 
 <!-- LIBRERIAS CSS -->
 	    <!-- Bootstrap -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <!--<link href="font-awesome-4.6.3/css/font-awesome.min.css" rel="stylesheet">-->
+    
+    <link href="<?php echo RUTA ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo RUTA ?>assets/css/style.css" rel="stylesheet">
     <link href="<?php echo RUTA ?>assets/css/main.css" rel="stylesheet">
-<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.css">
+    <link href="<?php echo RUTA ?>assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo RUTA ?>assets/css/jquery-ui.css" rel="stylesheet">
+
 
 </head>
 <body>
     <div id="header">
-    <!--  <div class="container">
+    <div class="container">
     <div class="perfil"></div>
-	<div class="logo_ucsc"><img src="<?php //echo RUTA ?>assets/img/logo_ucsc.png"></div>
-    <div class="logo_pace"><img src="<?php //echo RUTA ?>assets/img/logo_pace.jpg"></div>
+	<div class="logo_ucsc"><img src="<?php echo RUTA ?>assets/img/logo_ucsc.png"></div>
+    <div class="logo_pace"><img src="<?php echo RUTA ?>assets/img/logo_pace.jpg"></div>
     </div>
-	-->
 	</div>
 	<div id="contents"><?= $contents ?></div>
     <div id="footer">
@@ -38,12 +38,13 @@ define('RUTA', $ruta);
 	</script>
 	<!-- LIBRERIAS JS-->
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?php echo RUTA ?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo RUTA ?>assets/js/jquery.validate.js"></script>
 	
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.1/jquery.validate.js"></script>
+
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.js"></script>
+    <script src="<?php echo RUTA ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo RUTA ?>assets/js/jquery-ui.js"></script>
     <script src="<?php echo RUTA ?>assets/js/general.js"></script>
     <script src="<?php echo RUTA ?>assets/js/plugin.js"></script>
     <script src="<?php echo RUTA ?>assets/js/proyectos.js"></script>
