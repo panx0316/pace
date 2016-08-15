0d513e0a44f5b8c50bb29dcdb410d790a819ff09
Francisco <francisco.cordero.c@gmail.com>
2016-08-15 17:54:20 -0300

    FINANCIERO :)

--- application/views/financiero.php
+++ application/views/financiero.php
@@ -34,24 +34,38 @@
         <th>Estrategia</th>
         <th>Componente</th>
         <th>Resultado</th>
+        <th>Tipo gasto</th>
         <th>Detalle gasto</th>
         <th>Monto programado</th>
         <th>Monto gastado</th>
         <th>Saldo</th>
         <th>estado</th>
     </tr> 
-    <?php foreach ($areas as $data_areas){ ?>
-    <?php if($data_areas->P_ID_PROYECTO == $data_proyectos->P_ID_PROYECTO){ ?>
+    <?php foreach ($gastos as $data_gastos){ ?>
+    <?php if($data_gastos->P_ID_PROYECTO == $data_proyectos->P_ID_PROYECTO){ ?>
 
     <tr>
-        <th><?php echo $data_areas->P_ABREVIACION_AREA ?></th>
-        <td>CeldaA</td>
-        <td>CeldaB</td>
-        <td>CeldaA</td>
-        <td>CeldaB</td>
-        <td>CeldaA</td>
-        <td>CeldaB</td>
-        <td>CeldaA</td>
+        <th><?php echo $data_gastos->P_ABREVIACION_AREA; ?></th>
+        <td><?php echo $data_gastos->P_NOMBRE_ACTIVIDAD; ?></td>
+        <td><?php echo "RESULTADO"; ?></td>
+        <td><?php echo $data_gastos->P_NOMBRE_CLASIFICACION_ITEM; ?></td>
+        <td><?php echo $data_gastos->P_DETALLE_ITEM ?></td>
+        <td><?php echo $data_gastos->P_COSTO_PLANIF ?></td>
+        <td><?php echo $data_gastos->P_COSTO ?></td>
+		
+		<?php $subtotal=(($data_gastos->P_COSTO_PLANIF)-($data_gastos->P_COSTO));
+		if($subtotal<0){
+			$total='Saldo en Contra';
+		}
+		if($subtotal==0){
+			$total='Saldo cero';
+		}
+		if($subtotal>0){
+			$total='Saldo a Favor';
+		}
+		?>
+        <td><?php echo $subtotal ?></td>
+        <td><?php echo $total ?></td>
     </tr>
 <?php } ?>
 <?php } ?>
